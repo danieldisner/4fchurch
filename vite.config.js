1559d663e5a9229b30a98e27c0e0b06fdfcc246c
Daniel Disner <danidisner@gmail.com>
2024-08-07 19:13:09 -0300

    Vite Build Fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    server: {
-        cors: {
-            origin: '*',
-        },
-    },
+export default defineConfig(({ command }) => ({
     plugins: [
         laravel({
             input: [
@@ -20,18 +15,32 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    // Configuração adicional para o caminho do manifest
     build: {
+        manifest: {
+            publicPath: '/build/',
+        },
         outDir: 'public/build',
-        manifest: true,
         rollupOptions: {
             input: {
-                app: 'resources/js/app.js',
+                'app': 'resources/js/app.js',
                 'member-form': 'resources/js/member-form.js',
                 'finances-form': 'resources/js/finances-form.js',
                 'export-report': 'resources/js/export-report.js',
                 'app-css': 'resources/css/app.css',
-                'finances-form-css': 'resources/css/finances-form.css'
+                'finances-form-css': 'resources/css/finances-form.css',
             }
-        }
+        },
+
     },
-});
+    server: command === 'serve' ? {
+        cors: {
+            origin: '*',
+        },
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost',
+        }
+    } : {},
+
+}));
